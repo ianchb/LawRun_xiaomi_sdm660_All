4fa0be6bea9f22b938f499c1ce05417571a0f979
Cheng Haobo <admin@cgtsoft.com>
2023-08-16 22:02:43 +0800

    Update hooks.c:fix an error

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2190,7 +2190,7 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *new_tsec)
 {
 	static u32 ksu_sid;
-	char​ ​*​secdata​;
+	char *secdata;
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
 	int nosuid = (bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID);
 	int rc,error;
